999230a930228034e8d6adcfc0740a9e3daedfc2
mmkarim <mksaikat123@gmail.com>
2017-12-30 13:48:45 +0700

    clean viewfiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :posts
 
   root 'static_pages#home'
-  get 'static_pages/home'
+  get 'home', to: 'static_pages#home'
   get 'photos/index'
   post 'photos/upload'
   get 'search', to: 'search#search'
