11939b6e8b278dc3b51b4d402493f8ad9159162a
mmkarim <mksaikat123@gmail.com>
2017-12-30 12:18:49 +0700

    Es Pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,9 @@
 class SearchController < ApplicationController
   def search
-
-
     if params[:q].nil?
       @post_results = []
     else
-      @post_results = Post.search params[:q]
+      @post_results = Post.search(params[:q]).page(params[:page])
     end
-
-    # binding.pry
   end
 end
---
+++ config/initializers/es_pagination.rb
@@ -0,0 +1,2 @@
+Kaminari::Hooks.init if defined?(Kaminari::Hooks)
+Elasticsearch::Model::Response::Response.__send__ :include, Elasticsearch::Model::Response::Pagination::Kaminari
