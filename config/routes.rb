1310ad3ebe44024e5d57f5c0d209fbc99895d0d4
mmkarim <mksaikat123@gmail.com>
2017-12-30 15:11:49 +0700

    Portfolio and about view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get 'home', to: 'static_pages#home'
+  get 'portfolio', to: 'static_pages#portfolio'
+  get 'about', to: 'static_pages#about'
+
   get 'photos/index'
   post 'photos/upload'
   get 'search', to: 'search#search'
